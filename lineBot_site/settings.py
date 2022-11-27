f670e1461c7ce69f22db01e8d165896dc5c94ff2
freewhale <freewhale0624@gmail.com>
2022-11-28 00:37:31 +0800

    Add allowed host[pythoneverywhere]

--- lineBot_site/settings.py
+++ lineBot_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kgjq!&*j4h0ynz)vrutckgu8-+cd2t5+@#w&*5sq$q1ildn2+$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['community.pythonanywhere.com']
 
 
 # Application definition
